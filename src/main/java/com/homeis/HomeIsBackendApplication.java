1c7decd7110a29ff1f06a8ce8bf311de98c41168
jsj046 <jsj046@naver.com>
2024-05-09 14:32:40 +0900

    [Add] Innit

---
+++ src/main/java/com/homeis/HomeIsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.homeis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeIsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeIsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeis/HomeIsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeIsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
