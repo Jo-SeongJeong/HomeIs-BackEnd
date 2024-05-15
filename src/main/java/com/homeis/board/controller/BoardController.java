3bdf8c267459c3fb17146e73aaa06b6e86537a2b
SeongJeong <jsj046@naver.com>
2024-05-15 20:49:32 +0900

    [Feat] 자유게시판 pagination, token 인증 및 Trascation

--- src/main/java/com/homeis/board/controller/BoardController.java
+++ src/main/java/com/homeis/board/controller/BoardController.java
@@ -1,7 +1,5 @@
 package com.homeis.board.controller;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
