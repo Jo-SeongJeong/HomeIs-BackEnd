f6034a49b14fa31549fb3021de6ab22a168e09c7
SeongJeong <jsj046@naver.com>
2024-05-15 22:42:17 +0900

    [Feat] Qna pagination, token 인증 및 Trascation

--- src/main/java/com/homeis/config/WebConfig.java
+++ src/main/java/com/homeis/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry
 		.addInterceptor(authInterceptor)
-		.addPathPatterns("/board/**");
+		.addPathPatterns("/board/**", "/qna/**");
 	}
 	
 	@Override
--- src/main/java/com/homeis/qna/controller/QnaController.java
+++ src/main/java/com/homeis/qna/controller/QnaController.java
@@ -1,18 +1,24 @@
 package com.homeis.qna.controller;
 
-import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.homeis.qna.dto.Qna;
 import com.homeis.qna.dto.QnaComment;
+import com.homeis.qna.dto.QnaPaginationResponse;
 import com.homeis.qna.model.service.QnaService;
+import com.homeis.util.JWTUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,23 +27,39 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class QnaController {
 	private final QnaService qnaService;
+	private final JWTUtil jwtUtil;
 	
-	@PostMapping("/list")
-	public ResponseEntity<List<Qna>> list(@RequestBody Qna qna) {
-		List<Qna> qnaList = qnaService.selectAll(qna);
-		return ResponseEntity.ok(qnaList);
+	@GetMapping("/list/{userId}")
+	public ResponseEntity<?> list(@PathVariable("userId") String userId, 
+			@RequestHeader("Authorization") String tokenHeader,
+			@RequestParam(value = "size", defaultValue = "10") int size,
+			@RequestParam(value = "page", defaultValue = "1") int page) {
+		//토큰이 유효하지 않은 경우
+		boolean isValid = jwtUtil.isValid(tokenHeader.substring(7));
+		if(!isValid) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰 만료, 잘못된 접근.");
+		
+		String tokenId = jwtUtil.getIdFromToken(tokenHeader.substring(7));
+		if(!userId.equals(tokenId)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("잘못된 접근입니다.");
+		
+		QnaPaginationResponse response = qnaService.selectAll(userId, size, page);
+		
+		return ResponseEntity.ok(response);
 	}
 	
-	@PostMapping("/question")
-	public ResponseEntity<Qna> question(@RequestBody Qna qna) {
-		Qna qnaDetail = qnaService.getQuestion(qna);
-		return ResponseEntity.ok(qnaDetail);
-	}
-	
-	@PostMapping("/answer")
-	public ResponseEntity<List<QnaComment>> detail(@RequestBody Qna qna) {
-		List<QnaComment> qnaCommentList = qnaService.getAnswer(qna);
-		return ResponseEntity.ok(qnaCommentList);
+	@GetMapping("/detail/{userId}/{id}")
+	public ResponseEntity<?> question(@PathVariable("userId") String userId, @PathVariable("id") int id, @RequestHeader("Authorization") String tokenHeader) {
+		//토큰이 유효하지 않은 경우
+		boolean isValid = jwtUtil.isValid(tokenHeader.substring(7));
+		if(!isValid) return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰 만료, 잘못된 접근.");
+		
+		String tokenId = jwtUtil.getIdFromToken(tokenHeader.substring(7));
+		if(!userId.equals(tokenId)) return ResponseEntity.status(HttpStatus.FORBIDDEN).body("잘못된 접근입니다.");
+		
+		Qna qna = qnaService.findById(id);
+		
+		if(qna == null) return ResponseEntity.status(404).body("없는 글입니다!");
+		
+		return ResponseEntity.ok(qna);
 	}
 	
 	@PostMapping("/insert-question")
@@ -62,7 +84,7 @@ public class QnaController {
 	public ResponseEntity<?> update(@RequestBody Qna qna) {
 		int result = qnaService.updateQna(qna);
 		
-		if(result == 0) return ResponseEntity.notFound().build();
+		if(result == 0) return ResponseEntity.status(404).body("수정 불가!");
 		
 		return ResponseEntity.ok().build();
 	}
