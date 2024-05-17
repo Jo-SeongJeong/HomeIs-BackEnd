852ea3840b8ae8c8b15bfab647d0b21b20f05756
jsj046 <jsj046@naver.com>
2024-05-17 16:00:39 +0900

    [Feat] map 조회수 증가, 조회, 댓글 및 좋아요 등록/ 삭제

--- src/main/java/com/homeis/config/WebConfig.java
+++ src/main/java/com/homeis/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry
 		.addInterceptor(authInterceptor)
-		.addPathPatterns("/user/**", "/board/**", "/qna/**", "/notice/**", "/loan/**");
+		.addPathPatterns("/user/**", "/board/**", "/qna/**", "/notice/**", "/loan/**", "/map/**");
 	}
 	
 	@Override
--- src/main/java/com/homeis/notice/controller/NoticeController.java
+++ src/main/java/com/homeis/notice/controller/NoticeController.java
@@ -2,7 +2,6 @@ package com.homeis.notice.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
