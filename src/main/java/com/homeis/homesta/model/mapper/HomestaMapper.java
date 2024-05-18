223be030d97b1df4a01a75f57c89fcda7ea345f5
SeongJeong <jsj046@naver.com>
2024-05-18 23:20:26 +0900

    [Feat] homesta CRUD mapper, service

--- src/main/java/com/homeis/homesta/model/mapper/HomestaMapper.java
+++ src/main/java/com/homeis/homesta/model/mapper/HomestaMapper.java
@@ -1,8 +1,34 @@
 package com.homeis.homesta.model.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.homeis.homesta.dto.Homesta;
+import com.homeis.homesta.dto.HomestaImage;
+import com.homeis.homesta.dto.HomestaLike;
 
 @Mapper
 public interface HomestaMapper {
-
+	public List<Homesta> selectAll(Map<String, Object> param);
+	/**
+	 * 조회한 목록에 대한 전체 
+	 * @param param
+	 * @return
+	 */
+	int totalRow(Map<String, Object> param);
+	public List<HomestaImage> selectImage(int homestaId);
+	public int increaseView(int id);
+	public Homesta findById(int id);
+	public int insertHomesta(Homesta homesta);
+	public int insertImage(HomestaImage homestaImage);
+	public int updateHomesta(Homesta homesta);
+	public int deleteImage(int id);
+	public int deleteHomesta(Map<String, Object> param);
+	public int insertLike(HomestaLike like);
+	public int increaseLike(int id);
+	public int deleteLike(HomestaLike like);
+	public int decreaseLike(int id);
+	
 }
--- src/main/java/com/homeis/homesta/model/service/HomestaServiceImpl.java
+++ src/main/java/com/homeis/homesta/model/service/HomestaServiceImpl.java
@@ -1,7 +1,16 @@
 package com.homeis.homesta.model.service;
 
-import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.homeis.homesta.dto.Homesta;
+import com.homeis.homesta.dto.HomestaImage;
+import com.homeis.homesta.dto.HomestaLike;
+import com.homeis.homesta.dto.HomestaPaginationResponse;
 import com.homeis.homesta.model.mapper.HomestaMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -10,5 +19,127 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class HomestaServiceImpl implements HomestaService {
 	private final HomestaMapper homestaMapper;
+
+	@Override
+	public HomestaPaginationResponse slelectAll(int size, int page, String category, String sort) {
+		Map<String, Object> param = new HashMap<>();
+		param.put("size", size);
+		param.put("page", page);
+		param.put("category", category);
+		param.put("sort", sort);
+		
+		HomestaPaginationResponse resp = new HomestaPaginationResponse();
+		List<Homesta> homestaList = homestaMapper.selectAll(param);
+		
+		if(homestaList.isEmpty()) return null;
+		
+		for(Homesta homesta : homestaList) {
+			homesta.setCreaeteTime(homesta.getCreaeteTime().substring(0, 10));
+			homesta.setImage(homestaMapper.selectImage(homesta.getId()));
+		}
+		
+		resp.setHomestaList(homestaList);
+		
+		//페이지네이션 정보 세팅
+		int totalRow = homestaMapper.totalRow(param);
+		int totalPages = ((totalRow-1)/size)+1; 
+		resp.setTotalPages(totalPages);
+		resp.setSize(size);
+		resp.setPage(page);
+		/*======================*/
+		
+		return resp;
+	}
+
+	@Override
+	@Transactional
+	public Homesta findById(int id) {
+		int isSucceed = homestaMapper.increaseView(id);
+		
+		if(isSucceed == 0) return null;
+		
+		Homesta homesta = homestaMapper.findById(id);
+		
+		homesta.setImage(homestaMapper.selectImage(id));
+		
+		homesta.setCreaeteTime(homesta.getCreaeteTime().substring(0, 16));
+		
+		return homesta;
+	}
+
+	@Override
+	@Transactional
+	public int insertHomesta(Homesta homesta) {
+		int isSucceed = homestaMapper.insertHomesta(homesta);
+		
+		if(isSucceed == 0) return 0;
+		
+		for(HomestaImage image : homesta.getImage()) {
+			isSucceed =  homestaMapper.insertImage(image);
+			
+			if(isSucceed == 0) return 0;
+		}
+		
+		return isSucceed;
+	}
+
+	@Override
+	@Transactional
+	public int updateHomesta(Homesta homesta) {
+		int isSucceed = homestaMapper.updateHomesta(homesta);
+		
+		if(isSucceed == 0) return 0;
+		
+		isSucceed = homestaMapper.deleteImage(homesta.getId());
+		
+		if(isSucceed == 0) return 0;
+		
+		for(HomestaImage image : homesta.getImage()) {
+			isSucceed = homestaMapper.insertImage(image);
+			
+			if(isSucceed == 0) return 0;
+		}
+		
+		return isSucceed;
+	}
+
+	@Override
+	@Transactional
+	public int deleteHomesta(int id, String userId) {
+		Map<String, Object> param = new HashMap<>();
+		param.put("id", id);
+		param.put("userId", userId);
+		
+		return homestaMapper.deleteHomesta(param);
+	}
+
+	@Override
+	@Transactional
+	public int increaseLike(int id, String userId) {
+		HomestaLike like = new HomestaLike();
+		like.setHomestaId(id);
+		like.setUserId(userId);
+		
+		int isSucceed = homestaMapper.insertLike(like);
+		
+		if(isSucceed == 0) return 0;
+		
+		
+		return homestaMapper.increaseLike(id);
+	}
+
+	@Override
+	@Transactional
+	public int decreaseLike(int id, String userId) {
+		HomestaLike like = new HomestaLike();
+		like.setHomestaId(id);
+		like.setUserId(userId);
+		
+		int isSucceed = homestaMapper.deleteLike(like);
+		
+		if(isSucceed == 0) return 0;
+		
+		return homestaMapper.decreaseLike(id);
+	}
 
 }
