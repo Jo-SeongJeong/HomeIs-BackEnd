dc70bcb0a1feaacc42dc218c5e001939a2fb4d7b
SeongJeong <jsj046@naver.com>
2024-05-19 20:59:34 +0900

    [Feat] homesta CRUD controller

--- src/main/java/com/homeis/homesta/model/service/HomestaServiceImpl.java
+++ src/main/java/com/homeis/homesta/model/service/HomestaServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.homeis.homesta.dto.Homesta;
@@ -21,10 +22,10 @@ public class HomestaServiceImpl implements HomestaService {
 	private final HomestaMapper homestaMapper;
 
 	@Override
-	public HomestaPaginationResponse slelectAll(int size, int page, String category, String sort) {
+	public HomestaPaginationResponse selectAll(int size, int page, String category, String sort) {
 		Map<String, Object> param = new HashMap<>();
 		param.put("size", size);
-		param.put("page", page);
+		param.put("offset", (page-1)*size);
 		param.put("category", category);
 		param.put("sort", sort);
 		
@@ -34,7 +35,7 @@ public class HomestaServiceImpl implements HomestaService {
 		if(homestaList.isEmpty()) return null;
 		
 		for(Homesta homesta : homestaList) {
-			homesta.setCreaeteTime(homesta.getCreaeteTime().substring(0, 10));
+			homesta.setCreateTime(homesta.getCreateTime().substring(0, 10));
 			homesta.setImage(homestaMapper.selectImage(homesta.getId()));
 		}
 		
@@ -62,19 +63,24 @@ public class HomestaServiceImpl implements HomestaService {
 		
 		homesta.setImage(homestaMapper.selectImage(id));
 		
-		homesta.setCreaeteTime(homesta.getCreaeteTime().substring(0, 16));
+		homesta.setCreateTime(homesta.getCreateTime().substring(0, 16));
 		
 		return homesta;
 	}
 
 	@Override
-	@Transactional
+	@Transactional(isolation = Isolation.SERIALIZABLE) // 동시성 오류 해결 위해 serializable 적용
 	public int insertHomesta(Homesta homesta) {
+		if(homesta.getImage() == null) return 0;
+		
 		int isSucceed = homestaMapper.insertHomesta(homesta);
 		
 		if(isSucceed == 0) return 0;
 		
+		int id = homestaMapper.selectLast();
+		
 		for(HomestaImage image : homesta.getImage()) {
+			image.setHomestaId(id);
 			isSucceed =  homestaMapper.insertImage(image);
 			
 			if(isSucceed == 0) return 0;
@@ -115,17 +121,14 @@ public class HomestaServiceImpl implements HomestaService {
 
 	@Override
 	@Transactional
-	public int increaseLike(int id, String userId) {
-		HomestaLike like = new HomestaLike();
-		like.setHomestaId(id);
-		like.setUserId(userId);
+	public int increaseLike(HomestaLike like) {
 		
 		int isSucceed = homestaMapper.insertLike(like);
 		
 		if(isSucceed == 0) return 0;
 		
 		
-		return homestaMapper.increaseLike(id);
+		return homestaMapper.increaseLike(like.getHomestaId());
 	}
 
 	@Override
