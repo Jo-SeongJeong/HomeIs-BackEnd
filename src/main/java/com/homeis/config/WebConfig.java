9d59a258c6b6ae1f4ae64334c8c3b3a93ba17f85
junseok-99 <angec5689@naver.com>
2024-05-22 14:19:06 +0900

    fix: CORS 에러 해결2

--- src/main/java/com/homeis/config/WebConfig.java
+++ src/main/java/com/homeis/config/WebConfig.java
@@ -27,11 +27,11 @@ public class WebConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		// 기능 완성하고 마지막에 최적화 하기
-		registry.addMapping("/auth/**")
-		.allowedOrigins("http://localhost:5173", "http://192.168.206.40:5173")
-		.allowedMethods("POST");
+//		registry.addMapping("/auth/**")
+//		.allowedOrigins("http://localhost:5173", "http://192.168.206.40:5173")
+//		.allowedMethods("POST");
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:5173", "http://192.168.206.40:5173").allowedHeaders("Authorization")
+		.allowedOrigins("http://localhost:5173", "http://192.168.206.40:5173")
 		.allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
 	}
 	
